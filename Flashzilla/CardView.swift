d4402bcc9d0a7845bdb82ba848aeeb481f40ad97
oliver <oliverhu.oh@gmail.com>
2024-08-15 14:16:38 -0700

    project updated - added accessiblity options

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -12,11 +12,24 @@ struct CardView: View {
     var removal: (() -> Void)? = nil
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                        ? .white
+                        : .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25)
+                            .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
 
             VStack {
